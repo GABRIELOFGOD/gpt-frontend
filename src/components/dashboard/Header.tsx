de73041cc3ff5b65b0f9ce23453bae3bc6b1bf3a
GABRIELOFGOD <gabrielayodele2021@gmail.com>
2024-12-05 15:58:34 +0100

    I WILL FOREVER BE GRATEFUL TO YOU LORD

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -22,13 +22,15 @@ const Header = () => {
   return (
     <div className="w-full p-3 bg-white flex justify-between rounded-2xl ">
       <p className="my-auto font-bold">{userProfileState?.name}</p>
-      <div className="my-auto cursor-pointer bg-primary text-white p-1 rounded-full">
+      <div
+        className="my-auto cursor-pointer bg-primary text-white p-1 rounded-full"
+        onMouseEnter={() => setProfileHover(true)}
+        onMouseLeave={() => setProfileHover(false)}
+      >
         <BiUserCircle size={20} />
         <div
           onClick={logout}
-          onMouseEnter={() => setProfileHover(true)}
-          onMouseLeave={() => setProfileHover(false)}
-          className={`absolute right-0 top-14 gap-2 cursor-pointer bg-white text-black p-2 ${profileHover ? "flex" : "hidden"}`}
+          className={`absolute right-1 top-12 gap-2 cursor-pointer bg-white text-black p-2 ${profileHover ? "flex" : "hidden"}`}
         >
           <div className="my-auto">
             <BiLogOut />
